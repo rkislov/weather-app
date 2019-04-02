f810240ce59ef6c27ae9a03159210f2f2950caa3
Roman Kislov <roman@kislovs.ru>
2019-04-02 16:35:14 +0500

    error handeling

--- app.js
+++ app.js
@@ -10,31 +10,36 @@ request(
   },
   (error, response) => {
     // console.log(response.body.currently)
-    if (error){
-        console.log('Unable to connect to weather service')
+    if (error) {
+      console.log("Unable to connect to weather service");
+    } else if (response.body.error) {
+      console.log("unable to find location");
     } else {
-           console.log(
-            response.body.daily.data[0].summary +
-                " It is curenntly " +
-                response.body.currently.temperature +
-                " deegres out.  There is a " +
-                response.body.currently.precipProbability +
-                "% chance of rain"
-            );
-        }
+      console.log(
+        response.body.daily.data[0].summary +
+          " It is curenntly " +
+          response.body.currently.temperature +
+          " deegres out.  There is a " +
+          response.body.currently.precipProbability +
+          "% chance of rain"
+      );
     }
-
-
+  }
 );
 
 //Geocoding
 
-// const geoUrl =
-//   "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1Ijoicmtpc2xvdiIsImEiOiJjanR6bW1laHYwMTZhNGVwOHFjazR0NmdxIn0.6C7eMkkVUptRSmccMaJ_CQ";
+const geoUrl =
+  "https://api.mapbox.com/geocoding/v5/mapbox.places/Yekaterinburg.json?access_token=pk.eyJ1Ijoicmtpc2xvdiIsImEiOiJjanR6bW1laHYwMTZhNGVwOHFjazR0NmdxIn0.6C7eMkkVUptRSmccMaJ_CQ";
 
-// request({ url: geoUrl, json: true }, (error, response) => {
-//   const latitude = response.body.features[0].center[0];
-//   const longitude = response.body.features[0].center[1];
-//   console.log(latitude);
-//   console.log(longitude);
-// });
+request({ url: geoUrl, json: true }, (error, response) => {
+  if (error) {
+    console.log("Unable to connect to mapbox service");
+  } else if (response.body.features.length === 0) {
+    console.log("no matching result for your query");
+  } else {
+    const latitude = response.body.features[0].center[0];
+    const longitude = response.body.features[0].center[1];
+    console.log(latitude, longitude);
+  }
+});
