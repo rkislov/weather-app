93af16ee33fc994f8d4482e67a208b21897a69dc
Roman Kislov <roman@kislovs.ru>
2019-04-05 08:54:10 +0500

    forecast and geocode function

---
+++ utils/forecast.js
@@ -0,0 +1,30 @@
+const request = require('request')
+
+
+const forecast = (latitude, longitude, callback) => {
+    const url = "https://api.darksky.net/forecast/72d59ec58db1c3025ca639dda1b0e769/" + latitude + "," + longitude + "?units=si&lang=ru";
+    request({
+            url: url,
+            json: true
+        },
+        (error, response) => {
+            // console.log(response.body.currently)
+            if (error) {
+                callback("Unable to connect to weather service", undefined);
+            } else if (response.body.error) {
+                callback("unable to find location", undefined);
+            } else {
+                callback(undefined,
+                    response.body.daily.data[0].summary +
+                    " It is curenntly " +
+                    response.body.currently.temperature +
+                    " deegres out.  There is a " +
+                    response.body.currently.precipProbability +
+                    "% chance of rain"
+                );
+            }
+        }
+    );
+}
+
+module.exports = forecast
