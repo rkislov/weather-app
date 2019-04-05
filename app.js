ba2f21e4e75a349615cb4b5abf9e92d4268effb0
Roman Kislov <roman@kislovs.ru>
2019-04-05 10:07:52 +0500

    destruction

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-const yarg = require('yargs')
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
@@ -9,17 +8,21 @@ const address = process.argv[2]
 if (!address) {
     console.log('Please provide address')
 } else {
-    geocode(address, (error, data) => {
+    geocode(address, (error, {
+        latitude,
+        longitude,
+        location
+    }) => {
         if (error) {
             return console.log(error)
         }
 
-        forecast(data.latitude, data.longitude, (error, forecastData) => {
+        forecast(latitude, longitude, (error, forecastData) => {
             if (error) {
                 return console.log(error)
             }
 
-            console.log(data.location)
+            console.log(location)
             console.log(forecastData)
         })
     })
--- utils/forecast.js
+++ utils/forecast.js
@@ -4,21 +4,21 @@ const request = require('request')
 const forecast = (latitude, longitude, callback) => {
     const url = "https://api.darksky.net/forecast/72d59ec58db1c3025ca639dda1b0e769/" + latitude + "," + longitude + "?units=si&lang=ru";
     request({
-            url: url,
+            url,
             json: true
         },
-        (error, response) => {
+        (error, {body}) => {
             if (error) {
                 callback('Unable to connect to weather service', undefined);
-            } else if (response.body.error) {
+            } else if (body.error) {
                 callback('unable to find location', undefined);
             } else {
                 callback(undefined,
-                    response.body.daily.data[0].summary +
+                    body.daily.data[0].summary +
                     " It is curenntly " +
-                    response.body.currently.temperature +
+                    body.currently.temperature +
                     " deegres out.  There is a " +
-                    response.body.currently.precipProbability +
+                    body.currently.precipProbability +
                     "% chance of rain"
                 );
             }
