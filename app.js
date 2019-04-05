4b4b308b18f4f107b2c095e79bc6350223e05b94
Roman Kislov <roman@kislovs.ru>
2019-04-05 09:38:44 +0500

    provide cli

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
+const yarg = require('yargs')
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 
-geocode('Moscow', (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
-})
 
+const address = process.argv[2]
 
-forecast(-75.7088, 44.1545, (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
-})
+if (!address) {
+    console.log('Please provide address')
+} else {
+    geocode(address, (error, data) => {
+        if (error) {
+            return console.log(error)
+        }
+
+        forecast(data.latitude, data.longitude, (error, forecastData) => {
+            if (error) {
+                return console.log(error)
+            }
+
+            console.log(data.location)
+            console.log(forecastData)
+        })
+    })
+}
